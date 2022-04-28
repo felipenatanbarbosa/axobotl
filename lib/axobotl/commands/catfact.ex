234b0667611288a724effcefc6fc59903688d250
felipenatanbarbosa <fnatan2000@gmail.com>
2022-04-28 08:38:02 -0300

    :sparkles: finished catfact command

--- lib/axobotl/commands/catfact.ex
+++ lib/axobotl/commands/catfact.ex
@@ -1,26 +1,28 @@
 defmodule Catfact do
   alias Nostrum.Api
 
-  def handle(msg \\ "!catfact 4") do
+
+  def handle(msg) do
     [_head | tail] = String.split(msg.content, " ", parts: 2)
 
     case tail do
       [] -> 
-        Api.create_message(msg.channel_id, "Uso: *!catfact <Número de fatos>*")
+        handle_request(msg.channel_id)
 
-      n when n > 1 -> 
-        handle_request(tail, msg.channel_id)
+      _ -> 
+        Api.create_message(msg.channel_id, "Uso: *!catfact*")
+
     end
   end
 
-  defp handle_response(reqs, channel_id, base_url \\ 'https://cat-fact.herokuapp.com/facts/') do
-    for x <- reqs do
-      base_url
-      |> HTTPoison.get!
-      |> Map.get(:body)
-      |> {:ok, res} = Poison.decode
+  defp handle_request(channel_id) do
+    {:ok, request} = {:ok, "https://cat-fact.herokuapp.com/facts/"} 
+    |> elem(1)
+    |> HTTPoison.get!
+    |> Map.get(:body)
+    |> Poison.decode!
+    |> Enum.fetch(0)
 
-      IO.puts(res["text"])
+    Api.create_message(channel_id, request["text"])
     end
-  end
 end
