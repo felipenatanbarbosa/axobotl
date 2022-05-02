2ac1e205eb5db6e62f2eedba00c57f9af7d59652
nunqi <61606327+nunqi@users.noreply.github.com>
2022-05-01 22:00:21 -0300

    Update consumer.ex

--- lib/axobotl/consumer.ex
+++ lib/axobotl/consumer.ex
@@ -12,7 +12,6 @@ defmodule Axobotl.Consumer do
     cond do
       msg.content == "!ping" -> Api.create_message(msg.channel_id, "pong!")
       String.starts_with?(msg.content, "!echo ")    ->  Echo.handle(msg)
-      String.starts_with?(msg.content, "!axolotl ") ->  Axolotl.handle(msg)
       String.starts_with?(msg.content, "!catfact")  ->  Catfact.handle(msg)
       String.starts_with?(msg.content, "!catchcat") ->  Catservice.handle(msg)
       String.starts_with?(msg.content, "!validate") ->  Validation.handle(msg)
